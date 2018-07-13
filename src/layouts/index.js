eab181cdb5445b06c83789f21f85334abb1877f8
RobertoNyentek <Roberto@nyentek.com>
2018-07-13 15:59:21 -0700

    dont break please?

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Nyentek" />
     <Navbar />
     <div>{children()}</div>
   </div>
