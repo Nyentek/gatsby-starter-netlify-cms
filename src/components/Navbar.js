4284632cf0ba1d11c1a91fc135aaf8b5c69db293
RobertoNyentek <Roberto@nyentek.com>
2018-07-13 17:11:40 -0700

    fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-import github from '../img/github-icon.svg'
-import Logo from '/Logo'
+import { Logo } from './Logo.js'
 
 const Navbar = () => (
 	<header id="header-site" className="lower">
