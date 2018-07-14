4f32887ed880e72036c1c478edd4f8ae540d09e2
RobertoNyentek <Roberto@nyentek.com>
2018-07-13 17:14:17 -0700

    ...

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export const Logo = () => (
   <svg className="combination-mark" version="1.1" baseProfile="full"
        xmlns="http://www.w3.org/2000/svg" role="img" width="180"
