db443b6561ee8315358903a1c9d96458e583b1a2
RobertoNyentek <Roberto@nyentek.com>
2018-07-16 12:47:12 -0700

    rawr

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,52 +3,57 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 
 export default class IndexPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+	render () {
+		const { data } = this.props
+		const { edges: posts } = data.allMarkdownRemark
 
-    return (
-      <section className="section">
-        <div className="container">
-          <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
-          </div>
-          {posts
-            .map(({ node: post }) => (
-              <div
-                className="content card"
-                style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                key={post.id}
-              >
-                <p>
-                  <Link className="has-text-primary" to={post.fields.slug}>
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <small>{post.frontmatter.date}</small>
-                </p>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button is-small" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </div>
-            ))}
-        </div>
-      </section>
-    )
-  }
+		return (
+			<section className="section">
+				<div className="global-splash">
+					<div className="splash-content">
+						<h1>Nyentek Blog</h1>
+					</div>
+				</div>
+				<div className="container">
+					<div className="content">
+						<h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+					</div>
+					{posts
+						.map(({ node: post }) => (
+							<div
+								className="content card"
+								style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
+								key={post.id}
+							>
+								<p>
+									<Link className="has-text-primary" to={post.fields.slug}>
+										{post.frontmatter.title}
+									</Link>
+									<span> &bull; </span>
+									<small>{post.frontmatter.date}</small>
+								</p>
+								<p>
+									{post.excerpt}
+									<br/>
+									<br/>
+									<Link className="button is-small" to={post.fields.slug}>
+										Keep Reading →
+									</Link>
+								</p>
+							</div>
+						))}
+				</div>
+			</section>
+		)
+	}
 }
 
 IndexPage.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
+	data: PropTypes.shape({
+		allMarkdownRemark: PropTypes.shape({
+			edges: PropTypes.array
+		})
+	})
 }
 
 export const pageQuery = graphql`
